08f331c6bcbd744102fc8a82522c210a24176d35
Jan Dantes <paglubogngaraw@gmail.com>
2014-04-13 04:37:33 +0800

    Create broker function
    - Submit form to express route using post
    - catch mongoose validation messages

--- app/modules/settings/references/broker/add.js
+++ app/modules/settings/references/broker/add.js
@@ -1,18 +1,29 @@
 'use strict';
 
-define(['app','./services'], function(app,Services) {
-        app.controller('BrokerAdd', [
-            '$scope',
-            '$location',
-            'BrokersFactory',
-            function($scope, $location, BrokersFactory) {
+define(['app', './services'], function(app, Services) {
+    app.controller('BrokerAdd', [
+        '$scope',
+        '$location',
+        'BrokersFactory',
+        function($scope, $location, BrokersFactory) {
 
-                $scope.broker = {};
+            $scope.broker = {};
 
-                $scope.submit = function(form) {
-                    BrokersFactory.create($scope.broker);
-                    console.log($scope.broker);
-                }
-            }
-        ]);
+            $scope.submit = function(form) {
+                BrokersFactory.create($scope.broker, function(err) {
+                    if (err) {
+                        for (var key in err.errors) {
+                            form[key].message = err.errors[key].message;
+                        }
+                    } else {
+                        $scope.clear();
+                    }
+                });
+            };
+
+            $scope.clear = function() {
+                $scope.broker = angular.copy({});
+            };
+        }
+    ]);
 });
