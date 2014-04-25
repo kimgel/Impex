cfb94a0d0dd6d6ba1bace063cb0ef86fda7b23af
Jan Dantes <paglubogngaraw@gmail.com>
2014-04-25 21:09:22 +0800

    Separated navigation directive

---
+++ app/directives/navigation.js
@@ -0,0 +1,27 @@
+'use strict';
+
+define(['app'], function(app) {
+
+/* Navigation states
+=================================================================== */ 
+    app.directive('activePath', [
+        '$rootScope',
+        '$location',
+        function($rootScope, $location) {
+            return {
+                restrict: 'A',
+                link: function(scope, element) {
+                    scope.location = $location;
+                    console.log(element.parent());
+                    scope.$watch('location.path()', function(currentPath) {  
+                        if (currentPath === element[0].attributes['href'].nodeValue) {
+                            element.parent().addClass('active');
+                        } else {
+                            element.parent().removeClass('active');
+                        }
+                    });
+                }
+            };
+        }
+    ]);
+});
