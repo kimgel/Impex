14cf2464cff6661d2afe078253d76a6f9e6e1273
Jan Dantes <paglubogngaraw@gmail.com>
2014-05-08 23:05:05 +0800

    Fixed linting issues

--- app/modules/auth/service.js
+++ app/modules/auth/service.js
@@ -1,94 +1,96 @@
 'use strict';
 
-define(['app'], function(app) {
-    app.factory('Auth',[
+define(['app'], function (app) {
+    app.factory('Auth', [
         '$location',
         '$rootScope',
         'Session',
         'UsersFactory',
         '$cookieStore',
-        function($location, $rootScope, Session, UsersFactory, $cookieStore) {
+        function ($location, $rootScope, Session, UsersFactory, $cookieStore) {
 
-/* Get currentUser from cookie
-=================================================================== */   
+            /* Get currentUser from cookie
+            =================================================================== */
             $rootScope.currentUser = $cookieStore.get('user') || null;
             $cookieStore.remove('user');
 
             return {
 
-/* Authenticate user
-=================================================================== */  
-                login: function(user, callback) {
+                /* Authenticate user
+                =================================================================== */
+                login: function (user, callback) {
                     var cb = callback || angular.noop;
 
                     return Session.save({
                         username: user.username,
                         password: user.password
-                    }, function(user) {
+                    }, function (user) {
                         $rootScope.currentUser = user;
                         return cb();
-                    }, function(err) {
+                    }, function (err) {
                         return cb(err);
                     }).$promise;
                 },
 
-/* Unauthenticate user
-=================================================================== */ 
-                logout: function(callback) {
+                /* Unauthenticate user
+                =================================================================== */
+                logout: function (callback) {
                     var cb = callback || angular.noop;
 
-                    return Session.delete(function() {
+                    return Session.delete(
+                        function () {
                             $rootScope.currentUser = null;
                             return cb();
                         },
-                        function(err) {
+                        function (err) {
                             return cb(err);
-                        }).$promise;
+                        }
+                    ).$promise;
                 },
 
-/* Create a new user
-=================================================================== */ 
-                createUser: function(user, callback) {
+                /* Create a new user
+                =================================================================== */
+                createUser: function (user, callback) {
                     var cb = callback || angular.noop;
 
                     return UsersFactory.save(user,
-                        function(user) {
+                        function (user) {
                             $rootScope.currentUser = user;
                             return cb(user);
                         },
-                        function(err) {
+                        function (err) {
                             return cb(err);
                         }).$promise;
                 },
 
-                
-/* Change password
-=================================================================== */ 
-                changePassword: function(oldPassword, newPassword, callback) {
+
+                /* Change password
+                =================================================================== */
+                changePassword: function (oldPassword, newPassword, callback) {
                     var cb = callback || angular.noop;
 
                     return UsersFactory.update({
                         oldPassword: oldPassword,
                         newPassword: newPassword
-                    }, function(user) {
+                    }, function (user) {
                         return cb(user);
-                    }, function(err) {
+                    }, function (err) {
                         return cb(err);
                     }).$promise;
                 },
 
-/* Gets all available info on authenticated user
-=================================================================== */ 
-                currentUser: function() {
+                /* Gets all available info on authenticated user
+                =================================================================== */
+                currentUser: function () {
                     return UsersFactory.get();
                 },
 
-/* Simple check to see if a user is logged in
-=================================================================== */ 
-                isLoggedIn: function() {
+                /* Simple check to see if a user is logged in
+                =================================================================== */
+                isLoggedIn: function () {
                     var user = $rootScope.currentUser;
                     return !!user;
-                },
+                }
             };
         }
     ]);
