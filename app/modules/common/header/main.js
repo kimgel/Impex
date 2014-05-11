9412bb8d6dd883d7685640fd6e1d774c2f47a461
Jan Dantes <paglubogngaraw@gmail.com>
2014-05-11 21:43:47 +0800

    used $state.go instead $location.path
    - for ui-router

--- app/modules/common/header/main.js
+++ app/modules/common/header/main.js
@@ -1,15 +1,18 @@
 'use strict';
 
-define(['app'], function(app) {
+define(['app'], function (app) {
     app.controller('LogOutCtrl', [
         '$scope',
+        '$state',
         '$location',
         'Auth',
-        function($scope, $location, Auth) {
-            $scope.logout = function() {
+        function ($scope, $state, $location, Auth) {
+            $scope.logout = function () {
                 Auth.logout()
-                    .then(function() {
-                        $location.path('/login');
+                    .then(function () {
+                        $state.go('login', null, {
+                            reload: true
+                        });
                     });
             };
         }
