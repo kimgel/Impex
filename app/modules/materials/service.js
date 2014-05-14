934714bcf18ccdbb4c261ae9ca9c8d822e0695a2
Jan Dantes <paglubogngaraw@gmail.com>
2014-05-14 15:57:06 +0800

    added code flag for search by code function

--- app/modules/materials/service.js
+++ app/modules/materials/service.js
@@ -4,8 +4,9 @@ define(['app'], function(app) {
     app.factory('MaterialsFactory', [
         '$resource',
         function($resource) {
-            return $resource('/api/material/:materialId', {
-                materialId: '@_id'                
+            return $resource('/api/material/:materialId:materialCode', {
+                materialId: '@_id',
+                materialCode: '@code'            
             }, {
                 update: {
                     method: 'PUT'
