d75b86739864c76dd0ae1a3804f8ddf2b6fa624d
Jan Dantes <paglubogngaraw@gmail.com>
2014-05-18 13:46:56 +0800

    updated side nav

--- app/modules/common/sidenav/main.js
+++ app/modules/common/sidenav/main.js
@@ -4,20 +4,7 @@ define(['app'], function (app) {
     app.controller('MainNavCtrl', [
         '$scope',
         function ($scope) {
-            $scope.initiateimportDd = [
-                {
-                    "text": "Planner",
-                    "ui_sref": "initiateimport_planner"
-                },
-                {
-                    "text": "Broker",
-                    "ui_sref": "initiateimport_broker"
-                },
-                {
-                    "text": "Assign Forwarder",
-                    "ui_sref": "initiateimport_assignforwarder"
-                }
-            ];
+            
         }
     ]);
 });
