05941e8c3c5ae0735bdab6199ce91a0122c9aa4d
Jan Dantes <paglubogngaraw@gmail.com>
2014-05-27 10:27:09 +0800

    removed unused test directives

--- app/config/init.js
+++ app/config/init.js
@@ -1,41 +1,6 @@
 'use strict';
 
 define(['app'], function (app) {
-
-    /* Navigation states
-    =================================================================== */
-    app.directive('activePath', [
-        '$rootScope',
-        '$location',
-        function ($rootScope, $location) {
-            return {
-                restrict: 'A',
-                link: function (scope, element) {
-                    scope.location = $location;
-                    scope.$watch('location.path()', function (currentPath) {
-                        var elementHref = element[0].attributes['href'].nodeValue;
-                        var rootPath = currentPath.split('/')[1] + currentPath.split('/')[2];
-                        var currenHref = elementHref.split('/')[1] + elementHref.split('/')[2];
-                        if (rootPath === currenHref) {
-                            element.parent().addClass('active');
-                        } else {
-                            element.parent().removeClass('active');
-                        }
-                    });
-                }
-            };
-        }
-    ]);
-    
-    /* Header and Navigation state
-    =================================================================== */
-    app.controller('IncludeCtrl', [
-        '$rootScope',
-        '$scope',
-        '$location',
-        'Auth',
-        function ($rootScope, $scope, $location, Auth) {
-            
-        }
-    ]);
+   
+   
 });
