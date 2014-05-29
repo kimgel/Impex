9f6da63cb04dfca8792f28d1cecf0348e5197514
Jan Dantes <paglubogngaraw@gmail.com>
2014-05-30 03:58:18 +0800

    added ng-upload module

--- app/config/scripts.js
+++ app/config/scripts.js
@@ -1,80 +1,87 @@
 require.config({
-    baseUrl: '/',
+    baseUrl: "/",
     paths: {
 
         /* Config
         =================================================================== */
-        'States': '/config/states',
-        'Dependency': '/config/dependency',
-        'Init': '/config/init',
-        'Templates': '/config/templates',
+        "States": "/config/states",
+        "Dependency": "/config/dependency",
+        "Init": "/config/init",
+        "Templates": "/config/templates",
 
         /* 3rd Party
         =================================================================== */
-        'angular': '/assets/bower_components/angular/angular.min',
-        'angular-resource': '/assets/bower_components/angular-resource/angular-resource.min',
-        'angular-cookies': '/assets/bower_components/angular-cookies/angular-cookies.min',
-        'angular-sanitize': '/assets/bower_components/angular-sanitize/angular-sanitize.min',
-        'angular-ui-router': '/assets/bower_components/angular-ui-router/release/angular-ui-router.min',
-        'angular-animate': '/assets/bower_components/angular-animate/angular-animate.min',
-        'angular-strap': '/assets/bower_components/angular-strap/dist/angular-strap.min',
-        'angular-strap-tpl': '/assets/bower_components/angular-strap/dist/angular-strap.tpl',
-        'angular-loading-bar': '/assets/bower_components/angular-loading-bar/build/loading-bar.min',
+        "ng-upload-shim": "/assets/bower_components/ng-file-upload/angular-file-upload-shim.min",
+        "angular": "/assets/bower_components/angular/angular.min",
+        "angular-resource": "/assets/bower_components/angular-resource/angular-resource.min",
+        "angular-cookies": "/assets/bower_components/angular-cookies/angular-cookies.min",
+        "angular-sanitize": "/assets/bower_components/angular-sanitize/angular-sanitize.min",
+        "angular-ui-router": "/assets/bower_components/angular-ui-router/release/angular-ui-router.min",
+        "angular-animate": "/assets/bower_components/angular-animate/angular-animate.min",
+        "angular-strap": "/assets/bower_components/angular-strap/dist/angular-strap.min",
+        "angular-strap-tpl": "/assets/bower_components/angular-strap/dist/angular-strap.tpl",
+        "angular-loading-bar": "/assets/bower_components/angular-loading-bar/build/loading-bar.min",
+        "ng-upload": "/assets/bower_components/ng-file-upload/angular-file-upload.min",
 
         /* Services
         =================================================================== */
-        'Auth': '/modules/auth/service',
-        'Session': '/modules/session/service',
-        'Users': '/modules/user/service',
-        'Brokers': '/modules/broker/service',
-        'Forwarders': '/modules/forwarder/service',
-        'Suppliers': '/modules/supplier/service',
-        'Materials': '/modules/materials/service',
-        'ShippingLines': '/modules/shippinglines/service',
-        'RegulatoryDocuments': '/modules/regulatorydocuments/service',
-        'Planners': '/modules/initiateimport/planner/service'
+        "Auth": "/modules/auth/service",
+        "Session": "/modules/session/service",
+        "Users": "/modules/user/service",
+        "Brokers": "/modules/broker/service",
+        "Forwarders": "/modules/forwarder/service",
+        "Suppliers": "/modules/supplier/service",
+        "Materials": "/modules/materials/service",
+        "ShippingLines": "/modules/shippinglines/service",
+        "RegulatoryDocuments": "/modules/regulatorydocuments/service",
+        "Planners": "/modules/initiateimport/planner/service"
 
     },
     shim: {
-        'app': {
+        "app": {
             deps: [
-                'angular',
-                'angular-resource',
-                'angular-cookies',
-                'angular-sanitize',
-                'angular-ui-router',
-                'angular-animate',
-                'angular-strap',
-                'Templates',
-                'angular-loading-bar'
+                "ng-upload-shim",
+                "angular",
+                "angular-resource",
+                "angular-cookies",
+                "angular-sanitize",
+                "angular-ui-router",
+                "angular-animate",
+                "angular-strap",
+                "Templates",
+                "angular-loading-bar",
+                "ng-upload"
             ]
         },
-        'angular-resource': {
-            deps: ['angular']
+        "angular-resource": {
+            deps: ["angular"]
         },
-        'angular-cookies': {
-            deps: ['angular']
+        "angular-cookies": {
+            deps: ["angular"]
         },
-        'angular-sanitize': {
-            deps: ['angular']
+        "angular-sanitize": {
+            deps: ["angular"]
         },
-        'angular-ui-router': {
-            deps: ['angular']
+        "angular-ui-router": {
+            deps: ["angular"]
         },
-        'angular-animate': {
-            deps: ['angular']
+        "angular-animate": {
+            deps: ["angular"]
         },
-        'angular-strap': {
-            deps: ['angular']
+        "angular-strap": {
+            deps: ["angular"]
         },
-        'Templates': {
-            deps: ['angular-strap']
+        "Templates": {
+            deps: ["angular-strap"]
         },
-        'angular-loading-bar': {
+        "angular-loading-bar": {
             deps: [
-                'angular',
-                'angular-animate'
+                "angular",
+                "angular-animate"
             ]
+        },
+        "ng-upload": {
+            deps: ["angular"]
         }
     }
 });
