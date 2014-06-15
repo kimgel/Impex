d36e98a91ce1e0a6eb5b34e539aa20d6ac5ecc5a
Jan Dantes <paglubogngaraw@gmail.com>
2014-06-15 14:03:42 +0800

    renamed states to 'administration' added tooltip for table actions removed unused parameters

--- app/modules/forwarder/add.js
+++ app/modules/forwarder/add.js
@@ -1,6 +1,6 @@
 'use strict';
 
-define(['app', 'Forwarders'], function(app, Forwarders) {
+define(['app', 'Forwarders'], function(app) {
     app.controller('ForwarderAdd', [
         '$scope',
         '$state',
@@ -15,7 +15,7 @@ define(['app', 'Forwarders'], function(app, Forwarders) {
                             form[key].message = err.errors[key].message;
                         }
                     }else{
-                        $state.go('settings_forwarder');
+                        $state.go('administration_forwarder');
                     }
                 });
             };
